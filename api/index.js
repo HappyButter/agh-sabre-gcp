3f6d6b1a8373fde02ae1348df2e317db16a69464
HappyButter <mikolaj.marchewa@gmail.com>
2022-12-22 05:30:37 +0100

    Init api commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
