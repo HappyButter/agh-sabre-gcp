1ec37783ed7b1ea9f22edac60dc9f2a8f318584d
HappyButter <mikolaj.marchewa@gmail.com>
2023-01-04 22:38:29 +0100

    list scheduler locations endpoint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 import express from 'express'
-import {GoogleAuth} from 'google-auth-library';
+import { GoogleAuth } from 'google-auth-library';
+// import { CloudSchedulerClient } from '@google-cloud/scheduler';
+import { google }  from 'googleapis';
 
+const cloudscheduler = google.cloudscheduler('v1');
 const auth = new GoogleAuth();
+// const schedulerClient = new CloudSchedulerClient();
+
+const projectName = 'sabre-winter-course-2022';
 const app = express()
 
 app.use(express.json());
@@ -20,6 +26,34 @@ app.get('/vm/list', async (req, res) => {
 		const response = await client.request({url});
 	
 		res.status(200).json(response.data);
+	} catch (err) {
+		console.log(err);
+		res.status(400).send(err);
+	}
+})
+
+app.get('/vm/schedule/list', async (req, res) => {
+	try {
+		let aggregatedResponse = [];
+
+		const request = {
+			name: `projects/${projectName}`,
+			auth: auth,
+		  };
+		
+		  let response;
+		  do {
+			if (response && response.nextPageToken) {
+			  request.pageToken = response.nextPageToken;
+			}
+
+			response = (await cloudscheduler.projects.locations.list(request)).data;
+			const currentLocationsNames = response?.locations?.map(el => el.name) || [];
+			aggregatedResponse = [...aggregatedResponse, ...currentLocationsNames];
+			
+		  } while (response.nextPageToken);
+	
+		res.status(200).json(aggregatedResponse);
 	} catch (err) {
 		console.log(err);
 		res.status(400).send(err);
